c8ee0a985143c41500fa6bbcbd342de7b640eb20
Beau Kinstler <git@beaukinstler.info>
2018-07-21 15:21:33 -0400

    bug: flask needs the SECRET_KEY to be named just that

--- config.py
+++ config.py
@@ -6,3 +6,8 @@ class Config(object):
     with open('secrets/secrets.json', 'r') as json_file:
         json_key = json.loads(json_file.read())['super_secret_key']
     SUPER_SECRET_KEY = os.environ.get('SUPER_SECRET_KEY') or json_key
+    # Flask requires a constant call "SECRET_KEY", so I'm setting two
+    # In other projects I plan to adapt to this, I've used SUPER_ so I'm
+    # just keeping both as an option, so that both can be used, but 
+    # it will be the same key. 
+    SECRET_KEY = SUPER_SECRET_KEY
