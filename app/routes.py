059532cd9147821c612f9378ade6c0e00551809b
Beau Kinstler <git@beaukinstler.info>
2018-07-21 15:41:01 -0400

    refactor: change the form back to the blog example, and add flash messages

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template_string, render_template, request
+from flask import render_template_string, render_template, request, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -20,9 +20,9 @@ def index():
 
 @app.route('/login',methods=['GET', 'POST'])
 def login():
-    if request.method != 'POST':
-        form = LoginForm()
-        return render_template('login.html', title="Sign In", form=form)
-    else:
-        print(request.form['username'])
-        return str("POST found")
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login requested from {}, and remember_me  = {}".format(
+                form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
