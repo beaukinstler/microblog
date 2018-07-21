900fadbb27621cac8b32498087ab922cba3556b9
Beau Kinstler <git@beaukinstler.info>
2018-07-21 15:22:55 -0400

    test: Setup the login route and allow for post methods

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template_string, render_template
+from flask import render_template_string, render_template, request
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +17,12 @@ def index():
         }
     ]
     return render_template('index.html',user=user,title='Home',posts=posts)
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    if request.method != 'POST':
+        form = LoginForm()
+        return render_template('login.html', title="Sign In", form=form)
+    else:
+        print(request.form['username'])
+        return str("POST found")
