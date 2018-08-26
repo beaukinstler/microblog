aac5733948494e3fbe340dc7be5041f770cc0432
Beau Kinstler <beaukinstler@protonmail.com>
2018-08-26 18:56:15 +0000

    refactor: add support for the Posts data table

--- microblog.py
+++ microblog.py
@@ -1,5 +1,10 @@
-from app import app
+from app import app, db
+from app.models import User, Post
 import json
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
 
 
 SUPER_SECRET_KEY_OLD = (
