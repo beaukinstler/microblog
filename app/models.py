a52bfa1168e81ff8ea8f921521534c97874e2790
Beau Kinstler <beaukinstler@protonmail.com>
2018-08-30 20:24:45 +0000

    refactor: add password functions to User model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,6 +17,12 @@ class User(db.Model):
         For user with username susan, it will look like "<User susan>"
         """
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
