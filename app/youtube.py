e2b3d10bb34c465e489fdebf0f77d5b7c037e6a1
Beau Kinstler <git@beaukinstler.info>
2018-09-03 22:45:41 -0400

    initial testing script

---
+++ app/youtube.py
@@ -0,0 +1,33 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+def getmp3(link='https://www.youtube.com/watch?v=gn2933vMylY', quality='192'):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': quality,
+        }],
+        'logger': MyLogger(),
+        'progress_hooks': [my_hook],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
