664033a0cdfb91e57823f7405c0e97e3e7ec5947
Beau Kinstler <git@beaukinstler.info>
2018-09-13 11:38:13 -0400

    Add registrations page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template_string, render_template, request, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app.models import User
@@ -11,7 +11,7 @@ from app.models import User
 @app.route('/index')
 @login_required
 def index():
-    """ 
+    """
     Test data for initial testing phase
     """
     # user = {'username': 'Beau'}
@@ -37,7 +37,7 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash("Umm, that's not correct")
             return redirect(url_for('login'))
-        
+
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
@@ -47,3 +47,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register',methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("You're now one of us...")
+        return redirect(url_for('login'))
+    return render_template('register.html', title="Register", form=form)
