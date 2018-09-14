ad79c29306e5c26c183e298c2fc84d738e22dd3d
Beau Kinstler <git@beaukinstler.info>
2018-09-14 14:04:54 -0400

    chore: code pep8 cleaning

--- app/__init__.py
+++ app/__init__.py
@@ -18,4 +18,4 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 
-from app import routes, models
+from app import routes, models   # noqa
--- config.py
+++ config.py
@@ -11,14 +11,14 @@ class Config(object):
     SUPER_SECRET_KEY = os.environ.get('SUPER_SECRET_KEY') or json_key
     # Flask requires a constant call "SECRET_KEY", so I'm setting two
     # In other projects I plan to adapt to this, I've used SUPER_ so I'm
-    # just keeping both as an option, so that both can be used, but 
-    # it will be the same key. 
+    # just keeping both as an option, so that both can be used, but
+    # it will be the same key.
     SECRET_KEY = SUPER_SECRET_KEY
-    
+
     # Database config
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-            ('sqlite:///' + os.path.join(basedir, 'app.db'))
-    
+        ('sqlite:///' + os.path.join(basedir, 'app.db'))
+
     """
     Do not use the TRACK MODIFICATIONS to send a signal to the app
     when a database change is about to be made
