be0b00ceddd177e857392f6b7921d4f844dfd198
Beau Kinstler <git@beaukinstler.info>
2018-09-14 14:04:06 -0400

    feature: more user details and edit profile functions

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 from app import login
 
@@ -17,8 +18,9 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(256))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-    
-    
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
     def __repr__(self):
         """
         The __repr__ method tells Python how to print objects of this class,
@@ -26,12 +28,18 @@ class User(UserMixin, db.Model):
         For user with username susan, it will look like "<User susan>"
         """
         return '<User {}>'.format(self.username)
-    
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-        
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+                digest, size)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,17 @@
-from flask import render_template_string, render_template, request, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user, login_required
-from werkzeug.urls import url_parse
+# from werkzeug.urls import url_parse
 from app.models import User
+from datetime import datetime
 
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route('/')
@@ -14,20 +21,20 @@ def index():
     """
     Test data for initial testing phase
     """
-    # user = {'username': 'Beau'}
     posts = [
         {
-            'author': {'username':'Beau'},
-            'body':"Beautiful day in Brooklyn, NY"
+            'author': {'username': 'Beau'},
+            'body': "Beautiful day in Brooklyn, NY"
         },
         {
-            'author': {'username':'Uaeb'},
+            'author': {'username': 'Uaeb'},
             'body': 'It\'s a real treat'
         }
     ]
-    return render_template('index.html',title='Home',posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
-@app.route('/login',methods=['GET', 'POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -42,6 +49,7 @@ def login():
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/logout')
 @login_required
 def logout():
@@ -49,7 +57,7 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route('/register',methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -63,6 +71,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title="Register", form=form)
 
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
@@ -73,6 +82,7 @@ def user(username):
     ]
     return render_template('user.html', user=user, posts=posts)
 
+
 @app.route('/user')
 @app.route('/user/')
 @login_required
@@ -82,3 +92,20 @@ def user_redirect():
     else:
         flash("Please log in")
         return redirect(url_for('login'))
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
