297ff6b6e11673a94177f0ea3a9e0b54f44ef8e4
Beau Kinstler <git@beaukinstler.info>
2018-09-22 16:33:26 -0400

    refactor: make the pagination links

--- app/routes.py
+++ app/routes.py
@@ -22,6 +22,11 @@ def index():
     page = request.args.get("page", 1, type=int)
     posts = current_user.followed_posts().\
         paginate(page, app.config['POSTS_PER_PAGE'], False)
+    #  Pagination links
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
 
     if form.validate_on_submit():
         post = Post(body=form.post_body.data, author=current_user)
@@ -31,7 +36,8 @@ def index():
         return redirect(url_for('index'))
     else:
         return render_template('index.html', title='Home',
-                               form=form, posts=posts.items)
+                               form=form, posts=posts.items,
+                               next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -76,7 +82,15 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/user')
@@ -158,4 +172,10 @@ def explore():
     page = request.args.get("page", 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).\
         paginate(page, app.config['POSTS_PER_PAGE'], False)
-    return render_template('index.html', title='Explore', posts=posts.items)
+    #  Pagination links
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
