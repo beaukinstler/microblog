0f95873304ac3bb82eb8e480a36f5b1b8b37c2f2
Beau Kinstler <git@beaukinstler.info>
2018-10-07 20:42:36 -0400

    experiment: see if it makes sense to have generic form html
    this test proves that with WTF_Forms, python and Bootstrap, we can
    get away wiht have a generic form html, and just pass the form
    object created from the forms.py.  In this case, I added a title
    attribute that can be used to make the 'title' that's passed to
    the html.  Because title is used in the base html, it doesn't
    allow me to no pass it.

--- app/forms.py
+++ app/forms.py
@@ -33,6 +33,7 @@ class RegistrationForm(FlaskForm):
 
 
 class EditProfileForm(FlaskForm):
+    title = "Edit Profile"
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
