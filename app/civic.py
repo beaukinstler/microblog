c1404d5e0fca14845d94f18108a19e96d132fef1
Beau Kinstler <git@beaukinstler.info>
2018-10-20 15:02:06 -0400

    refactor: show election names in kv tupple for the choices parameter

--- app/civic.py
+++ app/civic.py
@@ -2,6 +2,7 @@ import json
 import requests
 from app import app
 from bleach.sanitizer import Cleaner
+import os
 
 
 ELECTION_URL = "https://www.googleapis.com/civicinfo/v2/elections"
@@ -16,6 +17,15 @@ def get_elections():
     data = json.loads(raw_data.content.decode('utf-8-sig'))
     elections = [e for e in data['elections'] if e['id'] not in '2000']
     return elections
+
+
+def get_election_names():
+    raw_data = requests.request('GET', ELECTION_URL,
+            params={'key': app.config['GOOGLE_CIVIC_KEY']})  # noqa
+    data = json.loads(raw_data.content.decode('utf-8-sig'))
+#     elections = [e for e in data['elections'] if e['id'] not in '2000']
+    elections = [e for e in data['elections']]  # for testing
+    return [(e['id'], e['name']) for e in elections]
 
 
 def get_polling_addresses(address, electionId):
