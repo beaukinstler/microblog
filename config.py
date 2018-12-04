28697de1670d5f8a7f8fb0749b2eb1eab2d2933f
Beau Kinstler <git@beaukinstler.info>
2018-12-04 17:22:33 -0500

    fix: get running on windows too

--- config.py
+++ config.py
@@ -12,56 +12,12 @@ class Config(object):
                 from the secrets file.
                 Preference is given the the 'secrets/secrets.json' file.
     """
-    json_key = ''
-    mail_server = ''
-    mail_port = ''
-    mail_uses_tls = ''
-    mail_username = ''
-    mail_password = ''
-    admin_email = ''
 
-    with open('secrets/secrets.json', 'r') as json_file:
-        json_blob = json.loads(json_file.read())
-
-        try:
-            json_key = json_blob['super_secret_key']
-        except Exception as e:
-            json_key = os.environ.get('SUPER_SECRET_KEY')
-
-        try:
-            mail_server = json_blob['mail_server']
-        except Exception as e:
-            mail_server = os.environ.get('MAIL_SERVER')
-
-        try:
-            mail_port = json_blob['mail_port']
-        except Exception as e:
-            mail_port = os.environ.get('MAIL_PORT')
-
-        try:
-            mail_uses_tls = json_blob['mail_uses_tls']
-        except Exception as e:
-            mail_uses_tls = os.environ.get('MAIL_USE_TLS')
-
-        try:
-            mail_username = json_blob['mail_username']
-        except Exception as e:
-            mail_username = os.environ.get('MAIL_USERNAME')
-
-        try:
-            mail_password = json_blob['mail_password']
-        except Exception as e:
-            mail_password = os.environ.get('MAIL_PASSWORD')
-
-        try:
-            admin_email = json_blob['admin_email']
-        except Exception as e:
-            admin_email = os.environ.get('ADMIN_EMAIL')
 
     # the other keys default to the json file.  This secret goes back to
     # to defering to the OS Environment instead.  Add similar logic to
     # do the same for other keys
-    SUPER_SECRET_KEY = os.environ.get('SUPER_SECRET_KEY') or json_key
+    SUPER_SECRET_KEY = os.environ.get('SUPER_SECRET_KEY')
 
     # Flask requires a constant call "SECRET_KEY", so I'm setting two
     # In other projects I plan to adapt to this, I've used `SUPER_` so I'm
@@ -79,12 +35,12 @@ class Config(object):
     """
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MAIL_SERVER = mail_server or 'localhost'
-    MAIL_PORT = int(mail_port or 25)
-    MAIL_USE_TLS = mail_uses_tls is not None
-    MAIL_USERNAME = mail_username
-    MAIL_PASSWORD = mail_password
-    ADMIN_EMAIL = admin_email
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
 
     #  Pagination
     POSTS_PER_PAGE = 25
