d5cd02a5a647055b8414c32954258ca9162fb759
Beau Kinstler <git@beaukinstler.info>
2019-03-05 14:17:52 -0500

    Update tranlate module with new env var and some doc string text

--- app/translate.py
+++ app/translate.py
@@ -4,12 +4,24 @@ from flask import current_app
 from flask_babel import _
 
 
-def translate(text, source_language, dest_language):
-    if 'MS_TRANSLATOR_KEY' not in current_app.config or \
-            not current_app.config['MS_TRANSLATOR_KEY']:
+def translate(text, source_language='en', dest_language='es'):
+    """Function to use the MS Azure Test Translate API
+    
+    Arguments:
+        text {string} -- text to translate
+        source_language {string} -- source language
+        dest_language {[type]} -- new translation language
+        NOTE: languages must be a valid language, e.g. 'en' or 'es' etc.
+    
+    Returns:
+        [json] -- returns json in utf-8 format with transated text
+    """
+
+    if 'MS_TRANSLATE_KEY' not in current_app.config or \
+            not current_app.config['MS_TRANSLATE_KEY']:
         return _('Error: the translation service is not configured.')
     auth = {
-        'Ocp-Apim-Subscription-Key': current_app.config['MS_TRANSLATOR_KEY']}
+        'Ocp-Apim-Subscription-Key': current_app.config['MS_TRANSLATE_KEY']}
     r = requests.get('https://api.microsofttranslator.com/v2/Ajax.svc'
                      '/Translate?text={}&from={}&to={}'.format(
                          text, source_language, dest_language),
