398c9096db078cb2b60d95298cb12faa8edc2146
Beau Kinstler <git@beaukinstler.info>
2019-03-06 10:05:33 -0500

    chore: remove some unused imports

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, redirect, url_for, flash, request
-from werkzeug.urls import url_parse
+from flask import render_template, redirect, url_for, flash
+# from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
-from flask_babel import _
+# from flask_babel import _
 from app import db
 from app.auth import bp
 from app.auth.forms import LoginForm, RegistrationForm, \
